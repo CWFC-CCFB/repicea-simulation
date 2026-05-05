08e37d754231fdc6bfe3685f02e88b0a8cd27b7b
Mathieu Fortin <mathieu.fortin.re@gmail.com>
2026-05-05 14:14:40 -0400

    Adding the BasalAreaSmallerThanSubjectM2Provider interface

---
+++ src/repicea/simulation/covariateproviders/treelevel/BasalAreaSmallerThanSubjectM2Provider.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the repicea-simulation library.
+ *
+ * Copyright (C) 2026 His Majesty the King in right of Canada
+ * Author: Mathieu Fortin, Canadian Forest Service
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
+package repicea.simulation.covariateproviders.treelevel;
+
+/**
+ * This interface ensures the tree instance can provide the basal
+ * area of the trees with smaller diameter than its own dbh.
+ * @author Mathieu Fortin - May 2026
+ */
+public interface BasalAreaSmallerThanSubjectM2Provider {
+
+	/**
+	 * This method returns the basal area of all the trees with dbh smaller than this tree instance.<p>
+	 * A typical implementation is plot basal area (m2/ha) minus basal area of larger trees (m2/ha) 
+	 * minus tree basal area (m2/ha), the latter only affected by the area-related expansion factor 
+	 * and not the number of trees in the tree instance.
+	 * @return basal area in m2/ha
+	 */
+	public double getBasalAreaSmallerThanSubjectM2Ha();
+
+}
